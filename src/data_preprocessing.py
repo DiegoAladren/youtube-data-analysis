3b20af32a823114cbd21e8051f5b77dc3270c941
DiegoAladren <146423034+DiegoAladren@users.noreply.github.com>
2025-10-27 16:08:26 +0100

    Initial project structure and data cleaning notebook

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,11 +1,21 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 import os
 
-# Obtener la ruta absoluta del proyecto (un nivel arriba de src/)
-base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Settings to view all columns
+pd.set_option('display.max_columns', None)
 
+# Seaborn aesthetic configuration
+sns.set(style='whitegrid')
+
+# Absolute path of the project
+base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 csv_path = os.path.join(base_dir, "data", "USvideos.csv")
 
+# Load the dataset
 df = pd.read_csv(csv_path)
 
-print(df.head(10))
+# Show the first lines
+print(df.head())
